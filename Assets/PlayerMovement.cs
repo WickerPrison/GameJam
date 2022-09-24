3a7dc4b9d6b7603d03877046fd24058f3da1623f
WickerPrison <laxislife29@gmail.com>
2022-09-23 22:54:01 -0600

    Player art and animations

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -10,6 +10,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float walkSpeed;
     [SerializeField] GameObject speechBubble;
     [SerializeField] TextMeshProUGUI dialogueText;
+    Animator animator;
+    Vector3 initialScale;
 
     private void Awake()
     {
@@ -23,12 +25,25 @@ public class PlayerMovement : MonoBehaviour
         controls.Gameplay.Walk.canceled += ctx => moveDir = 0;
 
         speechBubble.SetActive(false);
+
+        animator = GetComponentInChildren<Animator>();
+        initialScale = animator.transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
+        animator.SetFloat("MoveDir", Mathf.Abs(moveDir));
         transform.Translate(new Vector2(moveDir * Time.deltaTime * walkSpeed, 0));
+
+        if(moveDir > 0)
+        {
+            animator.transform.localScale = initialScale;
+        }
+        else if(moveDir < 0)
+        {
+            animator.transform.localScale = Vector3.Scale(initialScale, new Vector3(-1, 1, 1));
+        }
     }
 
 
